ebdbcd21d255f4453504b3e87782c0963100c3f8
Lord Drago <clearrio1465@gmail.com>
2026-05-10 06:12:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,8 @@
-const playlists = [
-  [
-    {
-      trackId: "trk101",
-      artist: "Velvet Comet",
-      title: "Crimson Afterglow",
-      votes: 5,
-      bpm: 122
-    },
-    {
-      trackId: "trk102",
-      artist: "Neon Harbor",
-      title: "Static Horizon",
-      votes: 2,
-      bpm: 108
-    },
-    {
-      trackId: "trk103",
-      artist: "Lunar Arcade",
-      title: "Midnight Frequency",
-      votes: 4,
-      bpm: 128
-    }
-  ],
-  [
-    {
-      trackId: "trk201",
-      artist: "Solar Echo",
-      title: "Glass Skyline",
-      votes: 3,
-      bpm: 115
-    },
-    {
-      trackId: "trk202",
-      artist: "Velvet Comet",
-      title: "Satellite Hearts",
-      votes: 6,
-      bpm: 124
-    }
-  ]
-];
-
-function flattenPlaylists(playlists) {
-  if (!Array.isArray(playlists)) {
-    return [];
-  }
-
-  const flattened = [];
-
-  playlists.forEach((playlist, playlistIndex) => {
-    playlist.forEach((track, trackIndex) => {
-      flattened.push({
-        ...track,
-        source: [playlistIndex, trackIndex]
-      });
-    });
+function scoreTracks(tracks) {
+  return tracks.map(track => {
+    return {
+      ...track,
+      score: track.votes * 10 - Math.abs(track.bpm - 120)
+    };
   });
-
-  return flattened;
-}
-
-function scoreTracks () {
-  
 }
